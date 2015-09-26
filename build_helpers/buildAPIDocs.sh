13fd8d6dd69bdcf64b17cbbb4aaf0a530574f730
prince1809 <prince18pat@gmail.com>
2015-09-26 08:36:51 +0000

    Build dev server config

---
+++ build_helpers/buildAPIDocs.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+// -*- mode: js -*-
+"use strict"
+
+var fs = require('fs')
+var path = require('path')
--- site/webpack-prerender.config.js
+++ site/webpack-prerender.config.js
@@ -1,1 +1,26 @@
 var path = require('path');
+var webpack = require('webpack')
+//var resolvers = require('../build_helpers/resolvers');
+var isDev = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  entry: path.join(__dirname, 'renderPath.js'),
+  output : {
+    path: '__site_prerender__/',
+    filename: 'renderPath.js',
+    libraryTarget: 'commonjs2',
+  },
+  target: 'node',
+
+  module: {
+    loaders: [
+      {
+        test: /\.md$/,
+        loader: [
+          'html?{"minimize":false}',
+          path.join(__dirname,'../build_helpers/markdownLoader')
+        ].join('!')
+      }
+    ]
+  }
+}
