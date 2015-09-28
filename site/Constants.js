c5cb5d7b1ceb26c63f440459f85d1782fa23cf36
prince1809 <prince18pat@gmail.com>
2015-09-28 13:04:16 +0000

    Example site config

--- site/Constants.js
+++ site/Constants.js
@@ -3,3 +3,17 @@
 exports.Pages = {
   HOME: 'HOME'
 }
+
+exports.APIPages = {
+  TABLE_API: { location: 'api-table.html', title: 'Table'}
+}
+exports.ExamplePages = {
+  OBJECT_DATA_EXAMPLE: {
+    location: 'example-object-data.html',
+    title: 'With JSON Data',
+    description: 'A basic table example with two fixed columns, fed in some JSON data',
+  }
+};
+
+exports.DOCS_DEFAULT = exports.APIPages.TABLE_API;
+exports.EXAMPLES_DEFAULT = exports.ExamplePages.OBJECT_DATA_EXAMPLE;
--- site/IndexPage.js
+++ site/IndexPage.js
@@ -3,5 +3,63 @@
 require('./base.less');
 
 var Constants = require('./Constants');
-//var HomePage = require('./home/HomePage');
+var HomePage = require('./home/HomePage');
 //var TableAPIPage = require('./docs/TableAPIPage');
+var ExamplesPage = require('./examples/ExamplesPage');
+var React = require('react');
+
+
+var ExamplePages = Constants.ExamplePages;
+
+var IndexPage = React.createClass({
+
+  render(){
+    var browserInitScriptObj = {
+      __html: 'window.INITIAL_PROPS = ' + JSON.stringify(this.props) + ';\n'
+    };
+
+    return(
+      <html>
+      <head>
+        <meta charset="utf-8"/>
+        <title> React Js DataTable </title>
+      </head>
+      <body>
+        {this.state.renderPage && this._renderPage()}
+
+        <script dangerouslySetInnerHTML={browserInitScriptObj} />
+        <script src={this.props.files['main.js']}></script>
+      </body>
+      </html>
+    );
+  },
+
+  _renderPage(){
+    switch (this.props.location) {
+      case OtherPages.HOME.location:
+        return <HomePage />;
+      case APIPages.TABLE_API.location:
+        return <TableAPIPage />;
+      case APIPages.COLUMN_API.location:
+        return <ColumnAPIPage />;
+      case APIPages.COLUMNGROUP_API.location:
+        return <ColumnGroupAPIPage />;
+      }
+
+      for(var key in ExamplePages) {
+        if(ExamplePages.hasOwnProperty(key) &&
+          ExamplePages[key].location === this.props.location) {
+            return <ExamplesPage example={ExamplePages[key]} />;
+          }
+      }
+
+      throw new Error(
+        'Page of location' +
+          JSON.stringify(this.props.location) +
+          ' not found.'
+      );
+  }
+
+});
+
+module.exports = IndexPage;
