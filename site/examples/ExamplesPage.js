a358021fdcbac2c8f4bc04bc7403006f1c3b1a38
prince1809 <prince18pat@gmail.com>
2015-09-29 14:57:49 +0000

    example pages

--- site/examples/ExamplesPage.js
+++ site/examples/ExamplesPage.js
@@ -1,5 +1,7 @@
 "use strict";
 
+
+
 var ExamplePages = React.createClass({
   getInitialState(){
     return {
@@ -19,5 +21,54 @@ var ExamplePages = React.createClass({
   _renderPage(){
     // Require common FixedDataTable CSS.
 
+    switch(this.props.example){
+      case ExamplePages.OBJECT_DATA_EXAMPLE:
+        var ObjectDataExample = require('./ObjectDataExample');
+
+        return (
+          <TouchExampleWrapper {...this.state}>
+            <ObjectDataExample />
+          </TouchExampleWrapper>
+        );
+      case ExamplePages.RESIZE_EXAMPLE:
+        var ResizeExample = require('./ResizeExample');
+        return (
+          <TouchExampleWrapper {...this.state}>
+            <ResizeExample />
+          </TouchExampleWrapper>
+        );
+    }
+
   },
+
+  componentDidMount(){
+    this._update();
+    var win = window;
+    if(win.addEventListener){
+      win.addEventListener('resize', this._onResize, false);
+    }else if(win.attachEvent){
+      win.attachEvent('onresize',this._onResize);
+    }else {
+      win.onresize = this._onResize;
+    }
+  },
+
+  _onResize(){
+    clearTimeout(this._updateTimer);
+    this._updateTimer = setTimeout(this._update, 16);
+  },
+
+  _update(){
+    var win = window;
+
+    var widthoffset = win.innerWidth < 680 ? 0 : 240;
+
+    this.setState({
+      renderPage: true,
+      tableWidth: win.innerWidth - widthoffset,
+      tableHeight: win.innerHeight - 200
+    });
+  }
 });
+
+module.exports = ExamplePages;
