5cdf092e2b2ea43941a412219e8e9ddac02fa396
prince1809 <prince18pat@gmail.com>
2015-09-30 16:26:27 +0000

    Dev Server Config

--- build_helpers/startSiteDevServer.sh
+++ build_helpers/startSiteDevServer.sh
@@ -4,7 +4,7 @@ PATH=$(npm bin):$PATH
 
 rm -rf ./__site__
 rm -rf ./__site_prerender__
-#./build_helpers/buildAPIDocs.sh
+./build_helpers/buildAPIDocs.sh
 webpack --config "$PWD/site/webpack-prerender.config.js"
 ./build_helpers/buildSiteIndexPages.sh
-webpack-dev-server --config "$PWD/site/webpack-client.config.js" --no-info --content-base __site__
+webpack-dev-server --host 198.71.89.77 --config "$PWD/site/webpack-client.config.js" --no-info --content-base __site__
