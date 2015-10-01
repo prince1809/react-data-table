0ef9985347fbfdde5f54d15b031f4e21c00e823c
prince1809 <prince18pat@gmail.com>
2015-10-01 15:52:21 +0000

    Example Object page render

--- site/examples/ExampleHeader.js
+++ site/examples/ExampleHeader.js
@@ -1,1 +1,15 @@
 "use strict"
+
+var React = require('react');
+var Constants = require('../Constants');
+
+var ExampleHeader = React.createClass({
+  render() {
+    return (
+      <div className="ExampleHeader">
+      </div>
+    );
+  }
+});
+
+module.exports = ExampleHeader;
--- site/examples/ExamplesWrapper.js
+++ site/examples/ExamplesWrapper.js
@@ -1,1 +1,15 @@
 "use strict"
+
+var React = require('react');
+
+
+var ExamplesWrapper = React.createClass({
+  render(){
+    return (
+      <div className="examplesPage">
+      </div>
+    )
+  }
+});
+
+module.exports = ExamplesWrapper;
